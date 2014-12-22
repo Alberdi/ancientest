3c545f22bb879449cf775b97408e281b4a8cf4e0
Marcelino Alberdi <marcelino.alberdi@gmail.com>
2014-12-22 22:18:07 +0100

    Adds code to pass populate test.

--- src/country.py
+++ src/country.py
@@ -4,7 +4,7 @@ class Country():
 
     def add_area(self, list_of_points):
         if not self._check_inline(list_of_points):
-            self.area = list_of_points
+            self.area = Area(list_of_points)
 
     def _check_inline(self, list_of_points):
         if len(list_of_points) > 2:
@@ -20,3 +20,17 @@ class Country():
                         return False
                 before_point = point
         return True
+
+class Area():
+    def __init__(self, points):
+        self.points = points
+        self.residents = []
+
+    def add_resident(self, resident):
+        for r in self.residents:
+            if r.element == resident.element:
+                return
+        self.residents.append(resident)
+
+    def population(self):
+        return len(self.residents)
--- src/creature.py
+++ src/creature.py
@@ -1,3 +1,3 @@
 class Creature():
-	def __init__(self):
-		pass
+    def __init__(self):
+        self.element = None
