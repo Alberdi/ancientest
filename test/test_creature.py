32c82a9a9142439916eb9d23b5165a588ed5c78d
josecriane <josecriane@gmail.com>
2014-12-25 23:30:49 +0100

    Adds test_travel

--- test/test_creature.py
+++ test/test_creature.py
@@ -16,3 +16,17 @@ class TestCreature(unittest.TestCase):
 		self.assertEqual(self.c.where_do_i_live(), country1)
 		country2.area.add_resident(self.c)
 		self.assertEqual(self.c.where_do_i_live(), country2)
+		
+	#A criature can only travel to adjacent country
+	def test_travel(self):
+		country1 = country.Country()
+		country1.add_area([(0,0), (100,0), (100, 100)])
+		country2 = country.Country()
+		country2.add_area([(0,0), (100, 0), (100, -100)])
+		country3 = country.Country()
+		country3.add_area([(200,0), (500, 0), (200, 200)])
+		country1.area.add_resident(self.c)
+		self.c.travel(country2)
+		self.assertEqual(self.c.where_do_i_live(), c2)
+		self.c.travel(country3)
+		self.assertEqual(self.c.where_do_i_live(), c2)
